6fdba029d5daed6da7b2697c86b7fbaf2d333890
BRTharun <brtarunkumar18@gmail.com>
2023-03-17 22:07:54 +0530

    changed for condition in bubble sort

--- sorting_algorithms/bubble_sort.cpp
+++ sorting_algorithms/bubble_sort.cpp
@@ -4,7 +4,7 @@ using namespace std;
 
 int bubbleSort (vector<int> &arr,int n){
     for (int i=1;i<=n-1;i++){
-        for(int j=0;j<n-i-1;j++){
+        for(int j=0;j<=n-i-1;j++){
             if(arr[j]>arr[j+1]){
                 swap(arr[j],arr[j+1]);
             }
